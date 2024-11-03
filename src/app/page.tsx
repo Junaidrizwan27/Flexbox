9459caaf0e9edffc82f6f857e4049f5620e4d491
Junaidrizwan27 <junaid.rizwan27@gmail.com>
2024-11-03 02:48:05 -0800

    first commits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
-  return (<div className="flex flex-row gap-10 justify-center items-center min-h-screen">
-    <div className="flexbox bg-red-500 h-80 w-80"></div>
-      <div className="flexbox bg-blue-500 h-80 w-80"></div>
-      <div className="flexbox bg-green-500 h-80 w-80"></div>
-      <div className="flexbox bg-white h-80 w-80"></div>  
+  return (
+  <div><div className="flex flex-row gap-10 justify-center items-center min-h-screen">
+    <div className="flexbox bg-red-500 h-40 w-40"></div>
+      <div className="flexbox bg-gray-300 h-40 w-40"></div>
+      <div className="flexbox bg-purple-400 h-40 w-40"></div>
+        
     </div>
+    <div className="flex flex-row gap-80 items-center justify-center">
+    <div className="flexbox bg-black h-80 w-80"></div>
+    <div className="flexbox bg-purple-900 h-80 w-80"></div>
+    </div></div>
   );
 }
